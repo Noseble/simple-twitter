6ab845ec2bea526b3ca2520d703310a365d99486
Noseble <manydreamcometure@gmail.com>
2023-03-24 22:57:44 +0800

    Router Link

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,20 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
 import styled from "styled-components";
 import clsx from "clsx";
 
 // 共用元件
 import StyledNavItem from "components/StyledNavItem";
-import StyledAdminTweet from "components/StyledAdminTweet";
+
 // svg
 import { ReactComponent as Logo } from 'assets/icon/AcLogo.svg'
+
+
+
 const AdminPage = ({className}) => {
+
   return(
     <div className={clsx('web-container', className)}>
       <nav className="nav-column">
         <div className="top">
         <div className="website-logo"><Logo /></div>
-        <StyledNavItem navTitle={'推文清單'} className="selected" />
-        <StyledNavItem navTitle={'使用者列表'} />
+        <Link to={'tweets'} style={{ textDecoration: 'none' }}><StyledNavItem navTitle={'推文清單'} className='selected' /></Link>
+        <Link to={'users'} style={{ textDecoration: 'none' }}><StyledNavItem navTitle={'使用者列表'} /></Link>
         </div>
         <StyledNavItem navTitle={'登出'} />
       </nav>
@@ -27,7 +32,6 @@ const StyledAdminPage = styled(AdminPage)`
   .top{
     margin-top: 20px;
   }
-
 `
 
 export default AdminPage
