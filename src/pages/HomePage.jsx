25923ad040f495226abc2ec97ac1ae9e0b52dda2
shanyueel <wulingkevin0704@gmail.com>
2023-03-24 20:21:55 +0800

    nav-column area finished

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,27 @@ import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 import clsx from "clsx";
 
+import StyledButton from "components/StyledButton";
+import StyledNavItem from "components/StyledNavItem";
+import {ReactComponent as AcLogo} from 'assets/icon/AcLogo.svg'
+
 const HomePage = ({className}) => {
   return(
     <div className={clsx('web-container', className)}>
       <nav className="nav-column">
-        nav-column
+        <div className="nav-main">
+          <div className="aclogo">
+            <AcLogo className="website-logo" />
+          </div>
+          <div className="nav-list">
+            <StyledNavItem navTitle='首頁' />
+            <StyledNavItem navTitle='個人資料' />
+            <StyledNavItem navTitle='設定' />
+          </div>
+          <StyledButton className="filled" width='100%'>推文</StyledButton>
+        </div>
+
+        <StyledNavItem className='exit-nav-item' navTitle='登出' />
       </nav>
       <div className="main-container">
         <div className="main-header">
