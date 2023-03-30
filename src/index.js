f4cfd920f9547d617278c14b1bdc36d2afb931fc
Noseble <manydreamcometure@gmail.com>
2023-03-30 23:59:20 +0800

    Debug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ReactModal from 'react-modal';
-import './index.css';
+import 'index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
